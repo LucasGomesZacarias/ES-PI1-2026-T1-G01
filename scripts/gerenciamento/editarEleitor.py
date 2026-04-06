d35375e6f4c615025c3405ca52944d4129defa93
Felipe-conceicao2501 <luiz.fc1@puccampinas.edu.br>
2026-04-06 08:49:40 -0300

    sub menus em arquivo separados e import time funcionando, match and case funcionando

--- scripts/gerenciamento/editarEleitor.py
+++ scripts/gerenciamento/editarEleitor.py
@@ -1,5 +1,20 @@
 import os
+import time
 
 def editar():
     os.system ("cls")
     print (f"==========================================\nEditar Eleitor")
+
+def listar_eleitor():
+    os.system ("cls")
+    print (f"==========================================\nListar Eleitor")
+
+def cadastrar_eleitor():
+    os.system ("cls")
+    print (f"==========================================\nCadastrar Eleitor")
+
+def deletar_eleitor():
+    os.system ("cls")
+    print (f"==========================================\nDeletar Eleitor")
+
+    
--- scripts/gerenciamento/menus.py
+++ scripts/gerenciamento/menus.py
@@ -1,40 +1,44 @@
 import os
-from gerenciamento.editarEleitor import editar
-
+import editarEleitor
+import time 
 # separar submenus e munu principal em arquivos diferentes, para não ficar tão grande e confuso.
-def listar_eleitor():
-    os.system ("cls")
-    print (f"==========================================\nListar Eleitor")
-
-def cadastrar_eleitor():
-    os.system ("cls")
-    print (f"==========================================\nCadastrar Eleitor")
-
-def deletar_eleitor():
-    os.system ("cls")
-    print (f"==========================================\nDeletar Eleitor")
 
 def menu_principal():
     os.system ("cls")
-    op = int(input(f"==========================================\nMenu\n\n1.0 Gerenciamento\n2.0 Votação\n\nEscolha sua opção:"))
-    if op ==1:
-        os.system ("cls")
-        ger = int(input(f"==========================================\nMenu Gerenciamento\n\n1.0 Gerenciamento de Eleitores\n2.0 Voltar ao Menu Principal\n\nEscolha sua opção:"))
-        if ger == 1:
-            os.system ("cls")
-            gerele = int(input(f"==========================================\nMenu Gerenciamento de Eleitores\n\n1.0 Editar Eleitor\n2.0 Listar Eleitores\n3.0 Cadastrar Eleitor\n4.0 Deletar Eleitor\n5.0 Voltar ao Menu Principal\n\nEscolha sua opção:"))
-            if gerele == 1:
-                editar()
-            if gerele == 2:
-                listar_eleitor()
-            if gerele == 3:
-                cadastrar_eleitor()
-            if gerele == 4:
-                deletar_eleitor()
-            if gerele == 5:
-                menu_principal()
-        elif ger ==2:
-            os.system ("cls")
-            menu_principal()
+    try:
+        op = int(input(f"==========================================\nMenu\n\n1.0 Gerenciamento\n2.0 Votação\n\nEscolha sua opção:"))
+        match op:
+            case 1:
+                os.system ("cls")
+                ger = int(input(f"==========================================\nMenu Gerenciamento\n\n1.0 Gerenciamento de Eleitores\n2.0 Voltar ao Menu Principal\n\nEscolha sua opção:"))
+                match ger:
+                    case 1:
+                        os.system ("cls")
+                        gerele = int(input(f"==========================================\nMenu Gerenciamento de Eleitores\n\n1.0 Editar Eleitor\n2.0 Listar Eleitores\n3.0 Cadastrar Eleitor\n4.0 Deletar Eleitor\n5.0 Voltar ao Menu Principal\n\nEscolha sua opção:"))
+                        match gerele:
+                            case 1:
+                                editarEleitor.editar()
+                            case 2:
+                                editarEleitor.listar_eleitor()
+                            case 3:
+                                editarEleitor.cadastrar_eleitor()
+                            case 4:
+                                editarEleitor.deletar_eleitor()
+                            case 5:
+                                menu_principal()
+            case 2:
+                pass
+    except ValueError:
+        os.system('cls')
+        print('==========================================\nUtilize números por favor\nvoltando.')
+        time.sleep(1)
+        os.system('cls')
+        print('==========================================\nUtilize números por favor\nvoltando..')
+        time.sleep(1)
+        os.system('cls')
+        print('==========================================\nUtilize números por favor\nvoltando...')
+        time.sleep(1)
+        os.system('cls')
+        menu_principal()
 
 menu_principal()
