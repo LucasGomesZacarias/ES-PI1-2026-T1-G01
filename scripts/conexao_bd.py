3f95665519b8d9532aede075179882bd0a38611e
Augusto <augusto.hm@puccampinas.edu.br>
2026-04-08 09:47:57 -0300

    BDconnect

--- scripts/conexao_bd.py
+++ scripts/conexao_bd.py
@@ -2,26 +2,26 @@ import mysql.connector
 # Conexão com o banco
 conexao = mysql.connector.connect(
     host='localhost',
-    user='seu_usuario',
-    password='sua_senha',
-    database='nome_do_banco'
+    user='root',
+    password='Augusto0609@',
+    database='banco_dados_pi'
 )
 cursor = conexao.cursor()
-# ---------- POST: Inserir um novo usuário ----------
-def inserir_usuario(nome, cpf):
-    sql = "INSERT INTO eleitores (nome, cpf) VALUES (%s, %s)"  #definir para inserir o resto da colunas depois
-    valores = (nome, cpf)
+# ---------- POST: Inserir um novo candidato ----------
+def inserir_candidato(nome, partido, numero_candidato):
+    sql = "INSERT INTO candidatos (nome, partido, numero_candidato) VALUES (%s, %s, %s)"
+    valores = (nome, partido, numero_candidato)
     cursor.execute(sql, valores)
     conexao.commit()
-    print("Usuário inserido com ID:", cursor.lastrowid)
-# ---------- GET: Buscar todos os usuários ----------
-def listar_usuarios():
-    cursor.execute("SELECT id, nome, email FROM usuarios")
-    for (id, nome, cpf) in cursor.fetchall():
-        print(f"ID: {id}, Nome: {nome}, Email: {cpf}")
+    print("Candidato inserido com ID:", cursor.lastrowid)
+# ---------- GET: Buscar todos os candidatos ----------
+def listar_candidatos():
+    cursor.execute("SELECT id, nome, partido, numero_candidato FROM candidatos")
+    for (id, nome, partido, numero_candidato) in cursor.fetchall():
+        print(f"ID: {id}, Nome: {nome}, Partido: {partido}, Número do Candidato: {numero_candidato}")
 # Exemplo de uso
-inserir_usuario("Felipe", "12345678901")
-listar_usuarios()
+inserir_candidato("Felipe", "ABCD", 13)
+listar_candidatos()
 # Fechar conexão
 cursor.close()
 conexao.close()
